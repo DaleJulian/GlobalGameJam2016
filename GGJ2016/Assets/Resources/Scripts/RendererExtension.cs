7993e58f399e6a2fcef84d882a7d4564476e393c
MidBoss <m.trinidad@klab.com>
2016-01-30 12:36:41 +0800

    RendererExtension.IsVisibleFrom (): Added check to ignore object's own collider.

--- GGJ2016/Assets/Resources/Scripts/RendererExtension.cs
+++ GGJ2016/Assets/Resources/Scripts/RendererExtension.cs
@@ -7,12 +7,20 @@ public static class RendererExtensions
 	{
 		bool isInView;
 		bool isBlocked;
+		RaycastHit hitInfo;
 
 		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
 
 		isInView = GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
-		isBlocked = Physics.Linecast (camera.transform.position, renderer.transform.position);
+		isBlocked = Physics.Linecast (camera.transform.position, renderer.transform.position, out hitInfo);
+
+		if (isBlocked) {
+			// Ignore a hit to object's own collider.
+			isBlocked = !(renderer.GetComponent<Collider> () == hitInfo.collider);
+		}
 
 		return (isInView && !isBlocked);
 	}
+
+
 }
