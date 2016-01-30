c2c39a77b3aab0c979c140fb9dea5dc507b1cc07
MidBoss <m.trinidad@klab.com>
2016-01-30 11:52:15 +0800

    Added RendererExtensions.cs

---
+++ GGJ2016/Assets/Resources/Scripts/RendererExtension.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class RendererExtensions
+{
+	public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+	{
+		bool isInView;
+		bool isBlocked;
+
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+
+		isInView = GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+		isBlocked = Physics.Linecast (camera.transform.position, renderer.transform.position);
+
+		return (isInView && !isBlocked);
+	}
+}
